340a99a149cd136673a826fbb9b2f9d2ff389a47
ElisaMamolo <elisa.carlotta.mamolo@lego.com>
2022-02-23 13:13:22 +0100

    4 - add new food

--- src/App.js
+++ src/App.js
@@ -4,11 +4,26 @@ import foods from './foods.json';
 import { useState } from 'react';
 import { Card, Row, Col, Divider, Input, Button } from 'antd';
 import FoodBox from './components/FoodBox';
+import AddFoodForm from './components/AddFoodForm';
 
 function App() {
   const [foodValues, setFoods] = useState(foods);
+  const [foodValuesData, setFoodsData] = useState(foods);
+
+  function updateFoodsList(newCard) {
+    const updatedFoodValues = [...foodValues, newCard];
+    const updatedfoodValuesData = [...foodValuesData, newCard];
+
+    setFoodsData(updatedfoodValuesData);
+    setFoods(updatedFoodValues);
+  }
+
   return (
     <div className="App">
+      <Row>
+        <AddFoodForm addItem={updateFoodsList} />
+      </Row>
+
       <Row>
         {foodValues.map((item) => {
           return <FoodBox food={item} />;
