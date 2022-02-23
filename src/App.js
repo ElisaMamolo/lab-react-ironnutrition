4c1eee7e507825ab693059a41068a7bd5fbe5fe6
ElisaMamolo <elisa.carlotta.mamolo@lego.com>
2022-02-23 12:07:16 +0100

    2

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import './App.css';
 import foods from './foods.json';
 import { useState } from 'react';
 import { Card, Row, Col, Divider, Input, Button } from 'antd';
+import FoodBox from './components/FoodBox';
 
 function App() {
   const [foodValues, setFoods] = useState(foods);
   return (
     <div className="App">
       {foodValues.map((item) => {
-        console.log(item);
-        return (
-          <div key={Math.random()}>
-            <p>{item.name}</p>
-            <img src={item.image} width={200} alt="image" />
-          </div>
-        );
+        return <FoodBox food={item} />;
       })}
     </div>
   );
